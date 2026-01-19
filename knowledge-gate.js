57c9f77c2f3c705d92bac487e8d980497161c65e
Wambita <wambitafana@gmail.com>
2026-01-19 13:33:15 +0300

    Style : add style  for  thinking text and bubbles

--- knowledge-gate.js
+++ knowledge-gate.js
@@ -15,7 +15,7 @@ function addMessage(messageText, senderType) {
 // Welcome message when page loads
 document.addEventListener("DOMContentLoaded", function () {
   addMessage(
-    'Hello, I am your intelligent books recommendation chatbot "01Books".\nGive me a keyword!\n\nExample: Mathematics',
+     'Hello, I am your book recommendation chatbot!\nGive me a topic!\n\nExample: Adventure',
     "bot"
   );
 });
@@ -36,3 +36,78 @@ function sendMessage() {
   }
 }
 
+async function getBookRecommendation(query) {
+  try {
+    const url = `https://openlibrary.org/search.json?q=${encodeURIComponent(
+      query
+    )}&limit=5`;
+
+    showTypingIndicator();
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch book data");
+    }
+
+    const data = await response.json();
+    removeTypingIndicator();
+
+    if (!data.docs || data.docs.length === 0) {
+      return "No books found for that topic. Try another keyword.";
+    }
+
+    let message = "Your recommended reads:<br><br>";
+
+    data.docs.slice(0, 5).forEach((book) => {
+      const title = book.title || "Unknown Title";
+      const author = book.author_name
+        ? book.author_name.join(", ")
+        : "Unknown Author";
+      const year = book.first_publish_year || "Unknown Year";
+
+      message += `<strong>${title}</strong><br>`;
+      message += `Author: ${author}<br>`;
+      message += `Published: ${year}<br><br>`;
+    });
+
+    return message;
+  } catch (error) {
+    console.error(error);
+    return "Something went wrong while fetching book recommendations.";
+  } finally{
+    removeTypingIndicator();
+  }
+}
+function showTypingIndicator() {
+    if (document.getElementById("typing-indicator")) return;
+  const messages = document.getElementById("messages");
+
+  const typingDiv = document.createElement("div");
+  typingDiv.classList.add("typing");
+  typingDiv.id = "typing-indicator";
+
+  typingDiv.innerHTML = `
+    <span>Thinking</span>
+    <div class="dots">
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+    </div>
+  `;
+
+  messages.appendChild(typingDiv);
+  messages.scrollTop = messages.scrollHeight;
+}
+function removeTypingIndicator() {
+  const typingDiv = document.getElementById("typing-indicator");
+  if (typingDiv) {
+    typingDiv.remove();
+  }
+}
+
+
+document.getElementById("user-input").addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        sendMessage();
+    }
+});
